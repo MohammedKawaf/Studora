aeef44dd2739d10ea447f4b529e062362b9f69ab
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-05-10 17:33:21 +0200

    feat: add user registration endpoint

---
+++ server/controllers/authController.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
