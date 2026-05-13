d4b5926b4796ba92e94d901924cabaf289ddf0ab
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-05-13 11:33:16 +0200

    feat: connect frontend register page to backend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+import api from "../services/api";
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(response.data);
+      alert("Account created successfully!");
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+      alert("Registration failed");
+    }
+  };
+
   return (
     <div>
-      <h1>Register</h1>
-      <p>Register page is working.</p>
+      <h1>Create account</h1>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
