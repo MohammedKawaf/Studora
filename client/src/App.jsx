4344ff4c79d49f70df9197116505026642aeac74
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-05-14 11:56:03 +0200

    feat: add protected frontend routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,32 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Notes from "./pages/Notes";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/notes"
+        element={
+          <ProtectedRoute>
+            <Notes />
+          </ProtectedRoute>
+        }
+      />
 
       <Route path="/login" element={<Login />} />
 
       <Route path="/register" element={<Register />} />
-
-      <Route path="/notes" element={<Notes />} />
     </Routes>
   );
 }
