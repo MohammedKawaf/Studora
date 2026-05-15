22a52596998e0bca371effed74d603b33715715d
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-05-15 11:12:21 +0200

    feat: add home overview dashboard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,109 @@
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
 function Home() {
+  const [user, setUser] = useState(null);
+
+  const [courses, setCourses] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchUser = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await api.get("/auth/profile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setUser(response.data);
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
+  const fetchCourses = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await api.get("/courses", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setCourses(response.data);
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
+  const fetchNotes = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await api.get("/notes", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setNotes(response.data);
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
+  const fetchTasks = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await api.get("/tasks", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setTasks(response.data);
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+    fetchCourses();
+    fetchNotes();
+    fetchTasks();
+  }, []);
+
   return (
     <div>
       <Navbar user={true} />
 
-      <h1>Studora Dashboard</h1>
+      <h1>Studora Home</h1>
 
-      <p>Welcome to Studora.</p>
+      {user && <h2>Welcome back, {user.username}</h2>}
 
-      <p>Your student overview will appear here.</p>
+      <h2>Your Overview</h2>
+
+      <div>
+        <h3>Courses</h3>
+        <p>{courses.length}</p>
+      </div>
+
+      <div>
+        <h3>Notes</h3>
+        <p>{notes.length}</p>
+      </div>
+
+      <div>
+        <h3>Tasks</h3>
+        <p>{tasks.length}</p>
+      </div>
     </div>
   );
 }
