91f9ee66c1120c85657994316d93e10353778402
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-05-16 11:27:56 +0200

    feat: add calendar event system

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -35,7 +35,59 @@ const getEvents = async (req, res) => {
   }
 };
 
+const updateEvent = async (req, res) => {
+  try {
+    const { title, type, date, time, course } = req.body;
+
+    const event = await Event.findById(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({
+        message: "Event not found",
+      });
+    }
+
+    event.title = title;
+    event.type = type;
+    event.date = date;
+    event.time = time;
+    event.course = course;
+
+    const updatedEvent = await event.save();
+
+    res.status(200).json(updatedEvent);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteEvent = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({
+        message: "Event not found",
+      });
+    }
+
+    await event.deleteOne();
+
+    res.status(200).json({
+      message: "Event deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createEvent,
   getEvents,
+  updateEvent,
+  deleteEvent,
 };
--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -13,4 +13,25 @@ router.post("/", protect, createEvent);
 
 router.get("/", protect, getEvents);
 
+module.exports = router;const express = require("express");
+
+const router = express.Router();
+
+const {
+  createEvent,
+  getEvents,
+  updateEvent,
+  deleteEvent,
+} = require("../controllers/eventController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", protect, createEvent);
+
+router.get("/", protect, getEvents);
+
+router.put("/:id", protect, updateEvent);
+
+router.delete("/:id", protect, deleteEvent);
+
 module.exports = router;
