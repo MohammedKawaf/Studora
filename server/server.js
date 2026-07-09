481073e18fa4de6beca29a91b1eb79ffe091efcf
Mohammed Kawaf <mohammedkoav@gmail.com>
2026-07-09 17:33:01 +0200

    feat: setup Socket.IO for real-time messaging

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
@@ -14,6 +16,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -29,12 +40,20 @@ app.use("/api/grades", gradeRoutes);
 app.use("/api/friends", friendRoutes);
 app.use("/api/messages", messageRoutes);
 
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("Studora API is running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
